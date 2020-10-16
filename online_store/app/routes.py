6f29d22417716c0e70d8bee8512e980f4d0f47d2
mesaye addisu <mesayeaddisu@mesayes-MacBook-Air.local>
2020-10-16 18:42:46 -0400

    session 2

--- online_store/app/routes.py
+++ online_store/app/routes.py
@@ -5,8 +5,10 @@
 from flask import g, request
 from app import app
 import sqlite3
+from flask import render_template
 
 DATABASE = "online_store"
+
 
 def get_db():
     db = getattr(g, "_database", None)
@@ -14,25 +16,47 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 def get_all_users():
-    cursor = get_db().execute("select * from user", ())
+    cursor = get_db().execute("SELECT * FROM user", ())
     results = cursor.fetchall()
     cursor.close()
-    return results     
+    return results
+
+
+def update_user():
+    cursor = get_db().execute("UPDATE user", ())
+    results = cursor
+    cursor.close()
+    return results
+
+def delete_user():
+    cursor = get_db().execute("DELETE FROM user", ())
+    results = cursor
+    cursor.close()
+    return results
+
+def create_user():
+    cursor = get_db().execute("INSERT into user values", ())
+    results = cursor
+    cursor.close()
+    return results
+
 
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, "_database", None)  
+    db = getattr(g, "_database", None)
     if db is not None:
         db.close()
+
 
 @app.route('/')
 def index():
     return "Hello,World"
 
-@app.route('/aboutme')
-def aboutme():
+@app.route('/about')
+def about():
     return {
         "first_name": "Mesaye",
         "last_name": "Addisu",
@@ -40,13 +64,13 @@ def aboutme():
     }
 
 
-@app.route('/users' , methods=["GET", "POST"])
+@app.route('/users', methods=["GET", "POST", "PUT","DELETE"])
 def get_users():
-    #creating an output dictinory
+    # creating an output dictinory
     out = {"ok": True, "body": ""}
     body_list = []
     if "GET" in request.method:
-        #get_all_users() returns all ecords from the user table
+        # get_all_users() returns all records from the user table
         raw_data = get_all_users()
         for item in raw_data:
             temp_dict = {
@@ -55,10 +79,31 @@ def get_users():
                 "hobbies": item[2]
             }
             body_list.append(temp_dict)
-        out["body"] = body_list  
-        return out  
+        out["body"] = body_list
+
+        return out
+
+        return render_template(
+        "base.html", first_name=out["body"] [0].get("first_name"),
+                     last_name=out ["body"] [0].get("first_name"),
+                     hobbies=out ["body"] [0].get("first_name")
+                                        )
+        
+
     if "POST" in request.methods:
-    # create a new user
-        pass               
+       
+        # create a new user
+        pass
+    
+
+@app.route('/countdown/<int:number>')
+def countdown(number):
+    return"</br>".join([str(i) for i in range(number, 0, -1) ])
+
+
+@app.route('/agent')
+def agent():
+    user_agent = request.headers.get("User_Agent")
+    return "<p>Your user agent is %s</p>" % user_agent    
 
 
